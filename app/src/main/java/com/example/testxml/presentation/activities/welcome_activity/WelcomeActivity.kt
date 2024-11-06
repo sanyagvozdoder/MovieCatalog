59b287f897c680b9339819893cb199ec4d47cfd4
Aleksandr Shadrov <alexshadrov1712@gmail.com>
2024-11-06 19:59:48 +0700

    fixed welcome activity

--- app/src/main/java/com/example/testxml/presentation/activities/welcome_activity/WelcomeActivity.kt
+++ app/src/main/java/com/example/testxml/presentation/activities/welcome_activity/WelcomeActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.testxml.R
+import com.example.testxml.databinding.CoordinatorMainBinding
+import com.example.testxml.databinding.WelcomeActivityBinding
 import com.example.testxml.presentation.activities.sign_in_activity.SignInActivity
 import com.example.testxml.presentation.activities.sign_up_activity.SignUpActivity
 
@@ -13,15 +15,18 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.welcome_activity)
 
-        val loginButton = findViewById<Button>(R.id.loginButton)
+        val binding = WelcomeActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val loginButton = binding.loginButton
         loginButton.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
 
-        val registerButton = findViewById<Button>(R.id.registerButton)
+        val registerButton = binding.registerButton
         registerButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
